f2b4c2362f2acb99d5a6e97a78824bac093a08ef
Karan Sharma <karansharma6190@gmail.com>
2023-08-26 10:03:47 +0000

    added cluster version

--- cluster.tf
+++ cluster.tf
@@ -17,6 +17,7 @@ data "aws_subnet" "id" {
 resource "aws_eks_cluster" "cluster" {
   name     = "${var.cluster_name}-${data.aws_caller_identity.account.account_id}-${var.env}"
   role_arn = aws_iam_role.cluster-role.arn
+  version  = "1.25"
   vpc_config {
     subnet_ids              = [for s in data.aws_subnet.id : s.id]
     security_group_ids      = []
